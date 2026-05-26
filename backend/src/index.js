b440801aa8e35cfdef6c2cb33bd0b9988d8900a9
martinelgueda9 <martinelgueda9@gmail.com>
2026-05-25 21:14:48 -0400

    Se configura el index para iniciar el servidor

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import express from "express";
+import morgan from "morgan";
+import { AppDataSource, connectDB } from "./config/configDB.js";
+import { routerApi } from "./routes/index.routes.js";
+
+const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.get("/", (req, res) => {
+  res.send("¡Bienvenido a mi API REST con TypeORM!");
+});
+
+connectDB()
+  .then(() => {
+    routerApi(app);
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Servidor iniciado en http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error al conectar con la base de datos:", error);
+    process.exit(1);
+  });
