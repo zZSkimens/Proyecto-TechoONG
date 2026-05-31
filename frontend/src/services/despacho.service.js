90076d4938b12816220f42e9f0ea61546ae71310
martinelgueda9 <martinelgueda9@gmail.com>
2026-05-30 21:23:49 -0400

    se crea servicio de despacho con respectiva orden y comprobante

---
+++ frontend/src/services/despacho.service.js
@@ -0,0 +1,23 @@
+import { get, put } from './api.js';
+
+export async function getOrdenes(filtros = {}) {
+  const params = new URLSearchParams(filtros).toString();
+  const endpoint = params ? `/ordenes-despacho?${params}` : '/ordenes-despacho';
+  const response = await get(endpoint);
+  return response.data;
+}
+
+export async function getOrdenById(id) {
+  const response = await get(`/ordenes-despacho/${id}`);
+  return response.data;
+}
+
+export async function procesarDespacho(id, data) {
+  const response = await put(`/ordenes-despacho/${id}/despachar`, data);
+  return response.data;
+}
+
+export async function getComprobante(id) {
+  const response = await get(`/ordenes-despacho/${id}/comprobante`);
+  return response.data;
+}
