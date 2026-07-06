df867a25f620a090bdb162f604375ed6a2241f0b
martinelgueda9 <martinelgueda9@gmail.com>
2026-07-05 21:48:56 -0400

    cambio en config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
       },
     },
+  },
+  preview: {
+    host: true,
+    port: 4173,
   },
 })
