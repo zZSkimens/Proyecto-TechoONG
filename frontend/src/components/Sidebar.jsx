d2189dbefcf4ab2bd188f4b23a53c99b4d832bd7
martinelgueda9 <martinelgueda9@gmail.com>
2026-07-07 22:15:44 -0400

    fix error

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -103,10 +103,11 @@ const SECTIONS = [
             <polyline points="3.27 6.96 12 12.01 20.73 6.96" />
             <line x1="12" y1="22.08" x2="12" y2="12" />
           </svg>
-        ),
-      },
-  },
-  {
+       ),
+       },
+      ],
+   },
+   {
     label: 'Administración para Cuadrillas',
     links: [
       {
