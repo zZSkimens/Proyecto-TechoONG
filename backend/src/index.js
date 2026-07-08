f0857ae1b1a1b0f376ed75b5b58f7c53b787d86d
martinelgueda9 <martinelgueda9@gmail.com>
2026-07-07 20:57:05 -0400

    se importa cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import morgan from "morgan";
 import { AppDataSource, connectDB } from "./config/configDb.js";
 import { routerApi } from "./routes/index.routes.js";
 import { createInitialUsers } from "./config/initialSetup.js";
-
+import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
