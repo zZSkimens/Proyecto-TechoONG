c3de8337cd5e4ff80eb2b36b92d675b349e83780
martinelgueda9 <martinelgueda9@gmail.com>
2026-07-07 22:32:32 -0400

    fix error

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ import { routerApi } from "./routes/index.routes.js";
 import { createInitialUsers } from "./config/initialSetup.js";
 import cors from "cors";
 const app = express();
-app.use(cors());
+app.use(cors({credentials: true, origin: true}));
 app.use(express.json());
 app.use(morgan("dev"));
 app.get("/", (req, res) => {
